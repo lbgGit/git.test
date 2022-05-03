91df991b05debee9a04a594617bf44d8ec9c83bb
lbgGit <104808683+lbgGit@users.noreply.github.com>
2022-05-03 16:19:50 +0800

    pull commit

--- src/main/java/com/evecom/git/GitTest.java
+++ src/main/java/com/evecom/git/GitTest.java
@@ -17,5 +17,6 @@ public class GitTest {
         System.out.println("master git5");
         System.out.println("hot-fix git5");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
