d359359e2902b6507ff7dc302b9a8cb28fce0045
Lbf <894831195@qq.com>
2022-05-03 15:35:34 +0800

    second commit

--- src/main/java/com/evecom/git/GitTest.java
+++ src/main/java/com/evecom/git/GitTest.java
@@ -11,5 +11,6 @@ package com.evecom.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
