6139c3ffbd407ac3f45986b5eb7e98ac7bf7be84
Lbf <894831195@qq.com>
2022-05-03 15:31:47 +0800

    first commit

---
+++ src/main/java/com/evecom/git/GitTest.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2005, 2020, EVECOM Technology Co.,Ltd. All rights reserved.
+ * EVECOM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.evecom.git;
+
+/**
+ * @author Evan Li
+ * @created 2022/5/3 15:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
